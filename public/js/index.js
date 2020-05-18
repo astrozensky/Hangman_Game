64de19ad6db1bb464a1f47b513d464c89437f226
Alex <astro124578@gmail.com>
2020-05-18 08:49:47 -0600

    add input style and wordList data structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,119 @@
-$(document).ready( () => {
+const wordList = [
+    { 
+        title: "Actors",
+        words: [
+            {
+                name: "Robert De Niro",
+                hint: "Robert"
+            },
+            {
+                name: "Denzel Washington",
+                hint: "Denzel"
+            },
+            {
+                name: "Betty White",
+                hint: "Betty"
+            },
+            {
+                name: "Scarlett Johansson",
+                hint: "Scarlett"
+            },
+        ]
+    },
+    {
+        title: "Movies",
+        words: [
+            {
+                name: "The Godfather",
+                hint: "Godfather"
+            },
+            {
+                name: "Citizen Kane",
+                hint: "Cit Kane"
+            },
+            {
+                name: "The Wizard of Oz",
+                hint: "Not in Kansas anymore"
+            },
+            {
+                name: "The Silence of the Lambs",
+                hint: "Be quite lambs"
+            },
+            {
+                name: "Gladiator",
+                hint: "Gladiator"
+            }
+        ]
+    },
+    {
+        title: "Animals",
+        words: [
+            { 
+                name: "Red Panda",
+                hint: "Cute fluffy mammal"
+            },
+            {
+                name: "Axolotl",
+                hint: "aka Mexican walking fish"
+            },
+            {
+                name: "Brown Bear",
+                hint: "Bear"
+            },
+            {
+                name: "Panther",
+                hint: "Black Cat"
+            },
+            {
+                name: "Baboon",
+                hint: "Has a red butt"
+            },
+            {
+                name: "Condor",
+                hint: "Largest bird"
+            }
+        ]
+    },
+    {
+        title: "NFL Teams",
+        words: [
+            {
+                name: "Seattle Seahawks",
+                hint: "seattle seahawks"
+            },
+            {
+                name: "Chicago Bears",
+                hint: "Chicago bears"
+            },
+            {
+                name: "Kansas City Chiefs",
+                hint: "Chiefs"
+            },
+            {
+                name: "Buffalo Bills",
+                hint: "Buffalo Bills"
+            },
+            {
+                name: "Jacksonville Jaguars",
+                hint: "Jacksonville Jaguars"
+            }
+        ]
+    }
+];
+
+const category = "";
+
+$(document).ready( function() {
     // Add event listeners here
+    
+    // Input Button
+    $(".col > button").click(function() {
+        $(this).addClass("guessed-letter")
+    });
+
+    // Categories Button
+    $(".categories > div > button").click(function() {
+        alert("works");
+    });
 });
+
